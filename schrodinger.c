a77d8f7563f3d00ccfa1ebb4d7d0c868c7c594a9
aniruddb16 <67001136+aniruddb16@users.noreply.github.com>
2021-01-13 18:39:06 +0530

    Update schrodinger.c

--- schrodinger.c
+++ schrodinger.c
@@ -3,24 +3,29 @@
 int main()
 {
 	
-	int del_x =5, mass=1, omega=1;
-	double x[5]={-1,-0.5,0,0.5,1}; //Position Vector
-	double T[20][20]; //Kinetic Energy Matrix
-	double V[20][20]; //Potential Energy Matrix
-	double H[20][20]; //Hamiltonian Matrix
-	const double h = 6;//626070040e-34; //Planck's Constant
-	const double P = 3;//141592653589793238; 
+	int del_x =1, mass=1, omega=1, size=41
+;
+	double x[size]; //Position Vector
+	static double T[20][20]; //Kinetic Energy Matrix
+	static double V[20][20]; //Potential Energy Matrix
+	static double H[20][20]; //Hamiltonian Matrix
+	const double h = 6.626070040e-34; //Planck's Constant
+	const double P = 3.141592653589793238; //pi
 	int i;
 	int j;
 	
-	//Potential Energy Operator
-	for(i=0;i<del_x;i++)
+	for (i=0; i<size; i++ )
 	{
-		for(j=0;j<del_x;j++)
+		x[i]=i-(size-1)/2;
+	}
+	//Potential Energy Operator
+	for(i=0;i<size;i++)
+	{
+		for(j=0;j<size;j++)
 		{
 			if(i == j)
 			{
-				V[i][j]=0.5*mass*omega*omega*x[i]*x[i];
+				V[i][j]=0.0005*mass*omega*omega*x[i]*x[i];
 				//printf("%f", V[i][j]);
 			}
 			else
@@ -35,10 +40,10 @@ int main()
 	
 	//Kinetic Energy operator
 
-	for(i=0;i<del_x;i++)
+	for(i=0;i<size;i++)
 	{
 		{
-			for(j=0; j<del_x; j++)
+			for(j=0; j<size; j++)
 			{
 				if(i==j)
 				{
@@ -58,12 +63,12 @@ int main()
 	
 	//ADDING T AND V
 	
-	for (i=0; i < del_x; i++)
+	for (i=0; i < size; i++)
 	{
 	
-        for (j=0; j < del_x; j++) {
+        for (j=0; j < size; j++) {
             H[i][j] = T[i][j] + V[i][j];
-            printf("%f", H[i][j]);
+            printf("%.5e \t", H[i][j]);
         }
         printf("\n");
     }
